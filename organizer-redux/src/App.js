4c512ac24eead592f24da22e68c61f159dfe14b5
Willbe.S <vvillbes@gmail.com>
2018-10-11 17:42:45 +0300

    Added styles and some pages

--- organizer-redux/src/App.js
+++ organizer-redux/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Routes from './components/common/routes/Routes';
+import Navbar from './components/common/navbar/Navbar';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Starter App
+      <div>
+        <header className='jumbotron text-center margin-bottom-0'>
+          <h1>Todo App Project</h1>
+          <p>Created by Willbe.S</p>
+        </header>
+        <Navbar />
+        <div className="container margin-top-30">
+          <Routes />
+        </div>
+
+        <footer className='jumbotron text-center margin-bottom-0 margin-top-30'>
+          <p>Footer</p>
+        </footer>
       </div>
+
     );
   }
 }
